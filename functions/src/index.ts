fd7d6be857f78a2fca8728c8b6fae65a51b6d6ce
Jerry Henriquez <jerryhenriquez@gmail.com>
2019-04-16 19:54:04 +0200

    agregada modificacion para utilizar database, cambio sobre firestore incompleto

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,13 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+export const save = functions.https.onRequest((request, response) => {
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    return admin.firestore().ref('/monitor').push({ temperature: 20 , humidity: 30, pressure: 15 }).then( (snapshot: { ref: { toString: () => string; }; }) => response.redirect(303, snapshot.ref.toString()));
+});
