ef7cbea7ed25a4abe96017f150de833f7f08a797
Jerry Henriquez <jerryhenriquez@gmail.com>
2019-04-19 19:01:19 +0200

    agregada interface para valores, generados datos para firebase y agregado almacenamiento en cada collection

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,10 +3,28 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
+interface monitor {
+    temperature: number,
+    humidity: number
+    pressure: number,
+    datetime: string
+}
+
 //Collect temperature, humidity and pressure values and store them into the database
 export const save = functions.https.onRequest(async (request, response) => {
-    const values = request.body;
-    return response.send('Saved: ' + JSON.stringify(values));
+    //Get values
+    const values: monitor = request.body;
+    const datetime = new Date(values.datetime);
+    const t = { "value": values.temperature, "datetime": admin.firestore.Timestamp.fromDate(datetime) };
+    const h = { "value": values.humidity, "datetime": admin.firestore.Timestamp.fromDate(datetime) };
+    const p = { "value": values.pressure, "datetime": admin.firestore.Timestamp.fromDate(datetime) };
+
+    //Save values
+    await admin.firestore().collection('temperature').add(t);
+    await admin.firestore().collection('humidity').add(h);
+    await admin.firestore().collection('pressure').add(p);
+
+    return response.send('Saved');
 });
 
 //Return temperature value with datetime
