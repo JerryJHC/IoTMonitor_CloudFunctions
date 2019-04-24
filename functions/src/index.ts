56bf5e21b7af61d96b3b617c71bcaf308378e0d5
Jerry Henriquez <jerryhenriquez@gmail.com>
2019-04-24 19:28:47 +0200

    agregado interface para los valores individuales y funcion para convertir los datos de firestore a js

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
+//Represents monitor values
 interface monitor {
     temperature: number,
     humidity: number
@@ -10,12 +11,15 @@ interface monitor {
     datetime: string
 }
 
-function getNumber(value: any): number{
-    if( typeof value === 'string' ){
+//Parse string value to number
+function getNumber(value: any): number {
+    if (typeof value === 'string') {
         return parseFloat(value);
     }
     return value;
 }
+
+/// Input
 
 //Collect temperature, humidity and pressure values and store them into the database
 export const save = functions.https.onRequest(async (request, response) => {
@@ -34,20 +38,38 @@ export const save = functions.https.onRequest(async (request, response) => {
     return response.send('Saved');
 });
 
+///Output
+
+//Represent single value
+interface monitorValue {
+    value: number,
+    datetime: Date
+}
+
+//Transform Firestore values to json format
+function getMonitorValues(docs: FirebaseFirestore.QuerySnapshot): monitorValue[] {
+    let monitorValues: monitorValue[] = [];
+    docs.docs.map(doc => {
+        let s = doc.data();
+        monitorValues.push({ "value": s.value, "datetime": s.datetime.toDate() });
+    });
+    return monitorValues;
+}
+
 //Return temperature value with datetime
 export const temperature = functions.https.onRequest(async (request, response) => {
     const docs = await admin.firestore().collection('temperature').get();
-    return response.send(docs.docs.map(doc => doc.data()));
+    return response.send(getMonitorValues(docs));
 });
 
 //Return humidity value with datetime
 export const humidity = functions.https.onRequest(async (request, response) => {
     const docs = await admin.firestore().collection('humidity').get();
-    return response.send(docs.docs.map(doc => doc.data()));
+    return response.send(getMonitorValues(docs));
 });
 
 //Return pressure value with datetime
 export const pressure = functions.https.onRequest(async (request, response) => {
     const docs = await admin.firestore().collection('pressure').get();
-    return response.send(docs.docs.map(doc => doc.data()));
+    return response.send(getMonitorValues(docs));
 });
